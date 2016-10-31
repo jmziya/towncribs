2bbd8d3fc2a2295547ec07eff20f8e275e42edd3
Jacob Mziya <jacobmziya@gmail.com>
2016-10-31 09:37:30 +0200

    Kaya number what commit kaya. Komweko.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,21 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( font-awesome.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( slick.css )
+Rails.application.config.assets.precompile += %w( nouislider.css )
+Rails.application.config.assets.precompile += %w( jquery.fancybox.css )
+Rails.application.config.assets.precompile += %w( theme-color/default-theme.css )
+Rails.application.config.assets.precompile += %w( style.css )
+
+
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( slick.js )
+Rails.application.config.assets.precompile += %w( nouislider.js )
+Rails.application.config.assets.precompile += %w( jquery.mixitup.js )
+Rails.application.config.assets.precompile += %w( jquery.fancybox.pack.js )
+Rails.application.config.assets.precompile += %w( custom.js )
